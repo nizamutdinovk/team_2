b7e5e05246300b3e2c0c44dcac8e09c5433403d2
nizamutdinovk <nizamutdinov.ke@students.dvfu.ru>
2022-07-26 09:25:31 +1000

    add part of task 3

--- Команда_2/Команда_2.cpp
+++ Команда_2/Команда_2.cpp
@@ -1,20 +1,78 @@
-﻿// Команда_2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <cmath>
 
-#include <iostream>
+//task_3
+/*
+double** create_energy_matrix(double** matrix_v_2, const int m) {
+	double** energy_matrix = new double* [m];
+	for (int i = 0; i < m; i++) {
+		energy_matrix[i] = new double[m];
+	}
+
+	// построение матрицы энергий
+	for (int i = 0; i < m; ++i) {
+		for (int j = 0; j < m; ++j) {
+			double d = matrix_v_2[i][j];
+			if (d == 0)
+				energy_matrix[i][j] = 0;
+			else
+				energy_matrix[i][j] = round((1 / pow(d, 12) - 1 / pow(d, 6)) * 100) / 100;
+		}
+	}
+
+	return energy_matrix;
+}
+*/
 
 int main()
 {
     std::cout << "Hello World!\n";
+
+
+
+
+
+
+
+/*
+//task_3	
+	//создание двумерного массива
+	int m = 3;
+	double** matrix_v_2 = new double* [m];
+	for (int i = 0; i < m; i++) {
+		matrix_v_2[i] = new double[m];
+	}
+
+	//	matrix_v_2[m][m] = {{1, 2, 0.5}, {0.6, 0.7, 0.8}, {0.9, 0.65, 0.84}};
+	//	1 2 0.5 0.6 0.7 0.8 0.9 0.65 0.84
+	for (int i = 0; i < m; ++i) {
+		for (int j = 0; j < m; ++j) {
+			std::cin >> matrix_v_2[i][j];
+		}
+	}
+
+	// вывод входной матрицы
+	for (int i = 0; i < 3; ++i) {
+		for (int j = 0; j < 3; ++j) {
+			std::cout << matrix_v_2[i][j] << " ";
+		}
+		std::cout << "\n";
+	}
+
+	double** energy_matrix = create_energy_matrix(matrix_v_2, m);
+
+	//	вывод матрицы энергий
+	for (int i = 0; i < 3; ++i) {
+		for (int j = 0; j < 3; ++j) {
+			std::cout << energy_matrix[i][j] << " ";
+		}
+		std::cout << "\n";
+	}
+
+	for (int i = 0; i < m; i++) delete[] matrix_v_2[i];
+	delete[] matrix_v_2;
+
+	for (int i = 0; i < m; i++) delete[] energy_matrix[i];
+	delete[] energy_matrix;
+*/
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
